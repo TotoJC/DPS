4628248b8bb13cad050513867c079c6632966349
TotoJC <antoniodiana267@gmail.com>
2024-06-11 18:59:21 +0200

    Create User model

---
+++ DPS/Models/User.cs
@@ -0,0 +1,7 @@
+public class User
+{
+    private string _username;
+    private string _password;
+    public string Username { get => _username; set => _username = value; }
+    public string Password { get => _password; set => _password = value; }
+}
